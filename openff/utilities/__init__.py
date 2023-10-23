28e1c81162733d02e0db6bb76ac8b303eb89abc4
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-10-23 10:18:52 -0500

    MAINT: Update CI, fix typing error

--- openff/utilities/__init__.py
+++ openff/utilities/__init__.py
@@ -1,4 +1,4 @@
-from openff.utilities._version import get_versions  # type: ignore
+from openff.utilities._version import get_versions
 from openff.utilities.exceptions import MissingOptionalDependencyError
 from openff.utilities.provenance import get_ambertools_version
 from openff.utilities.testing import skip_if_missing, skip_if_missing_exec
@@ -11,7 +11,19 @@ from openff.utilities.utilities import (
     temporary_cd,
 )
 
-from . import _version
+__all__ = (
+    "get_versions",
+    "get_ambertools_version",
+    "has_executable",
+    "has_package",
+    "requires_oe_module",
+    "requires_package",
+    "skip_if_missing",
+    "skip_if_missing_exec",
+    "temporary_cd",
+    "MissingOptionalDependencyError",
+    "get_data_file_path",
+)
 
 versions = get_versions()
 __version__ = versions["version"]
