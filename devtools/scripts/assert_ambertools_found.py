87e94535ce96d648aa5f1354cf313c24431cb354
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-10-07 11:34:44 -0500

    Fix

--- devtools/scripts/assert_ambertools_found.py
+++ devtools/scripts/assert_ambertools_found.py
@@ -3,5 +3,6 @@ from shutil import which
 
 try:
     assert which('sqm')
-except AssertionError:
+except AssertionError as error:
     os.system(f"ls {os.environ['CONDA_PREFIX']}/bin/")
+    raise error
